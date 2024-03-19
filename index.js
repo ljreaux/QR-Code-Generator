46328d9e3450de5459c2e375c03f7219884c9145
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-03-19 18:04:47 +0000

    finished functionality

---
+++ index.js
@@ -0,0 +1,59 @@
+import inquirer from "inquirer";
+import select from '@inquirer/select'
+import qr from "qr-image"
+import fs from "fs"
+
+const getUserInput = async () => {
+    const url = await inquirer.prompt([{
+        message: "Please enter your link to generate a QR code:",
+        name: 'URL'
+    }]) 
+    const fileName = await inquirer.prompt([{
+        message: "Please enter a name for your new QR Code:",
+        name: 'fileName'
+    }]) 
+    const fileType = await select({
+        message: "Please select a file type for your QR Code:",
+        choices: [
+            {
+                name: "png",
+                value: "png"
+            },
+            {
+                name: "svg",
+                value: "svg"
+            },
+            {
+                name: "pdf",
+                value: "pdf"
+            },
+            {
+                name: "eps",
+                value: "eps"
+            }
+
+        ]
+    })
+    console.log({...url, ...fileName, type: fileType})
+    return {...url, ...fileName, type: fileType}
+}
+
+const createQR = async () => {
+    const userInput = await getUserInput();
+    const {URL, fileName, type} = userInput
+
+    const QR = qr.image(URL, {...type})
+
+    QR.pipe(fs.createWriteStream(`${fileName}.${type}`))
+    fs.writeFile(fileName, JSON.stringify(userInput), (error)=>{
+        if(error) throw error
+        try {
+            console.log("The file has been saved!");
+        }
+        catch (error){
+            console.error(error)
+        }
+    })
+}
+
+createQR()
