d29fdb370536b0a8e40ffe02f0b255a22835fd61
Larry Reaux <larryreaux@gmail.com>
2024-03-19 16:50:22 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # QR Code Generator
-This is a simple QR code generator using node.js. Allows you to enter a url and a file name using your CLI.
+
+This is a simple QR code generator using node.js. Allows you to enter a url, file name, and file type using your CLI. The file is then saved in the CWD.
--- bin/index.js
+++ bin/index.js
@@ -1,61 +1,63 @@
 #!/usr/bin/env node
 
 import inquirer from "inquirer";
-import select from '@inquirer/select'
-import qr from "qr-image"
-import fs from "fs"
+import select from "@inquirer/select";
+import qr from "qr-image";
+import fs from "fs";
 
 const getUserInput = async () => {
-    const url = await inquirer.prompt([{
-        message: "Please enter your link to generate a QR code:",
-        name: 'URL'
-    }]) 
-    const fileName = await inquirer.prompt([{
-        message: "Please enter a file name for your new QR Code:",
-        name: 'fileName'
-    }]) 
-    const fileType = await select({
-        message: "Please select a file type for your QR Code:",
-        choices: [
-            {
-                name: "png",
-                value: "png"
-            },
-            {
-                name: "svg",
-                value: "svg"
-            },
-            {
-                name: "pdf",
-                value: "pdf"
-            },
-            {
-                name: "eps",
-                value: "eps"
-            }
+  const url = await inquirer.prompt([
+    {
+      message: "Please enter your link to generate a QR code:",
+      name: "URL",
+    },
+  ]);
+  const fileName = await inquirer.prompt([
+    {
+      message: "Please enter a file name for your new QR Code:",
+      name: "fileName",
+    },
+  ]);
+  const fileType = await select({
+    message: "Please select a file type for your QR Code:",
+    choices: [
+      {
+        name: "png",
+        value: "png",
+      },
+      {
+        name: "svg",
+        value: "svg",
+      },
+      {
+        name: "pdf",
+        value: "pdf",
+      },
+      {
+        name: "eps",
+        value: "eps",
+      },
+    ],
+  });
 
-        ]
-    })
-    
-    return {...url, ...fileName, type: fileType}
-}
+  return { ...url, ...fileName, type: fileType };
+};
 
 const createQR = async () => {
-    const userInput = await getUserInput();
-    const {URL, fileName, type} = userInput
+  const userInput = await getUserInput();
+  const { URL, fileName, type } = userInput;
 
-    const QR = qr.image(URL, {...type})
+  const QR = qr.image(URL, { ...type });
 
-    QR.pipe(fs.createWriteStream(`${fileName}.${type}`))
-    fs.writeFile(`${fileName}.txt`, JSON.stringify(userInput), (error)=>{
-        if(error) throw error
-        try {
-            console.log("The file has been saved!");
-        }
-        catch (error){
-            console.error(error)
-        }
-    })
-}
+  QR.pipe(fs.createWriteStream(`${fileName}.${type}`));
+  fs.writeFile(`${fileName}.txt`, JSON.stringify(userInput), (error) => {
+    if (error) throw error;
+    try {
+      console.log("The file has been saved!");
+    } catch (error) {
+      console.error(error);
+    }
+  });
+};
 
-createQR()
+createQR();
